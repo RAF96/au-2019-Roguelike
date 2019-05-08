ab9be3e3c2e077673fa02502c085edb24cd5ba08
Irina Pluralia <irina@metrix.ai>
2019-05-08 14:57:18 +0300

    init

--- core/src/com/roguelike/softwaredesign/au2019/Grid.java
+++ core/src/com/roguelike/softwaredesign/au2019/Grid.java
@@ -2,7 +2,7 @@ package com.roguelike.softwaredesign.au2019;
 
 
 import java.util.ArrayList;
-import java.util.stream.IntStream;
+
 
 public class Grid {
     static Boolean MOSK = true;
@@ -12,9 +12,9 @@ public class Grid {
     public ArrayList<ArrayList<Integer>> to2dArray() {
         if (MOSK) {
             ArrayList<ArrayList<Integer>> grid = new ArrayList<ArrayList<Integer>>();
-            for (int i : IntStream.range(0, nrow).toArray()) {
-                grid.add(new ArrayList<>());
-                for (int j : IntStream.range(0, nrow).toArray()) {
+            for (int i = 0; i < nrow; i++) {
+                grid.add(new ArrayList<Integer>());
+                for (int j = 0; j < column; j++) {
                     grid.get(grid.size() - 1).add(0);
                 }
             }
